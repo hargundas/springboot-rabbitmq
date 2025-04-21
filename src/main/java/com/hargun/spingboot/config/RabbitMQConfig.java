e7bc40563df69fccea4c051ae64f6600ca140c5a
Hargun Das <hargun.das@paysyslabs.com>
2025-04-21 16:01:10 +0500

    added rabbitmq config for queue , exchange and there routings

---
+++ src/main/java/com/hargun/spingboot/config/RabbitMQConfig.java
@@ -0,0 +1,41 @@
+package com.hargun.spingboot.config;
+
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.name}")
+    private String rountingKey;
+
+    //rabbitmq bean  for rabbitmq queue
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+
+    //rabbitmq bean for rabbitmq exchange
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+
+    //binding between queue and  exchange using routing key
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(rountingKey);
+     }
+}
